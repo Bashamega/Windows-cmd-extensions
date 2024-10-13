78c51b0624c22e3e13162b79edc1d9fd33c3b4d0
Ritisha <ritish200302@gmail.com>
2024-10-13 19:46:05 +0530

    open list issue

--- windows-cmd-extensions/bats/open.bat
+++ windows-cmd-extensions/bats/open.bat
@@ -5,28 +5,35 @@ if "%~1"=="" (
 ) else (
     if /i "%~1" == "list" (
         echo.
-        echo This is the list of available applications in the Start menu:
+        echo This is the list of installed applications:
         echo -----------------------------------------------------------
-        
-        rem Query the registry for PinnedTiles
-        for /f "tokens=2,*" %%A in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\StartPage" /v "PinnedTiles" 2^>nul') do (
-            set "apps=%%B"
-        )
 
-        if "%apps%"=="" (
-            echo No applications found in the registry.
-        ) else (
-            rem Iterate over the found applications
-            for %%A in (%apps%) do (
-                rem Query AppPath for each app in the StartPage
-                for /f "tokens=2,*" %%C in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\StartPage\%%A" /v "AppPath" 2^>nul') do (
-                    if not "%%C"=="" echo %%C
+        rem Query the registry for installed applications
+        setlocal enabledelayedexpansion
+        set "foundApps="
+
+        rem Check both user and local machine uninstall keys
+        for %%A in (
+            "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall"
+            "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+        ) do (
+            for /f "tokens=2,*" %%B in ('reg query %%A /s /v "DisplayName" 2^>nul') do (
+                if not "%%C"=="" (
+                    echo %%C
+                    set "foundApps=1"
                 )
             )
         )
+
+        if not defined foundApps (
+            echo No applications found in the registry.
+        )
+
+        endlocal
     ) else (
         echo.
         echo Starting "%~1"
         start "" "%~1"
     )
 )
+
