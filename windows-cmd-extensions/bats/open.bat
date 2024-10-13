9998a1bd032f5be505355b0b1c87c862e0202b3f
Ritisha <ritish200302@gmail.com>
2024-10-13 19:13:09 +0530

    open list issue

--- windows-cmd-extensions/bats/open.bat
+++ windows-cmd-extensions/bats/open.bat
@@ -8,19 +8,25 @@ if "%~1"=="" (
         echo This is the list of available applications in the Start menu:
         echo -----------------------------------------------------------
         
-        reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\StartPage" /v "PinnedTiles" | findstr /i "MRUListEx Applications"
-        for /f "tokens=2,*" %%A in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\StartPage" /v "PinnedTiles" ^| findstr /i "MRUListEx Applications"') do (
+        rem Query the registry for PinnedTiles
+        for /f "tokens=2,*" %%A in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\StartPage" /v "PinnedTiles" 2^>nul') do (
             set "apps=%%B"
         )
-        
-        for %%A in (%apps%) do (
-            for /f "tokens=2,*" %%C in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\StartPage\%%A" /v "AppPath"') do (
-                echo %%C
+
+        if "%apps%"=="" (
+            echo No applications found in the registry.
+        ) else (
+            rem Iterate over the found applications
+            for %%A in (%apps%) do (
+                rem Query AppPath for each app in the StartPage
+                for /f "tokens=2,*" %%C in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\StartPage\%%A" /v "AppPath" 2^>nul') do (
+                    if not "%%C"=="" echo %%C
+                )
             )
         )
     ) else (
         echo.
         echo Starting "%~1"
-        start /B "" "%~1"
+        start "" "%~1"
     )
 )
